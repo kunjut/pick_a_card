4247848461c4a128628e16053720f0bd14901c26
Vadim Dimnich <v.dimnich@gmail.com>
2020-05-16 00:06:25 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 # программа выбрасывает рандомную карту из колоды
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %w(Diamonds Hearts Clubs Spades)
+suits = %w(Diamonds Hearts Clubs Spades) # Буби, Черви, Крести, Пики
 
 # вывод случайных значений из каждого массива
 puts "#{values.sample} of #{suits.sample}"
