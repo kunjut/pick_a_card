01558972dfa576e62f020b8ababbc500e817ff12
Vadim Dimnich <v.dimnich@gmail.com>
2020-05-16 00:24:44 +0300

    change output

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,4 +3,4 @@ values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 suits = %W[\u2662 \u2661 \u2667 \u2664] # Буби, Черви, Крести, Пики
 
 # вывод случайных значений из каждого массива
-puts "#{values.sample} of #{suits.sample}"
+puts values.sample << suits.sample
